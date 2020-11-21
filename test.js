1c8f172641317e909070f54015b2a6bdc67939d2
wuyao <wuyaoftd@163.com>
2020-11-21 22:41:26 +0800

    两数之和 map 方式

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] == target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
 var twoSum = function (nums, target) {
+  let myMap = new Map();
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        return [i, j];
-      }
+    let def = target - nums[i];
+    if (myMap.has(def)) {
+      return [myMap.get(def), i];
+    } else {
+      myMap.set(nums[i], i);
     }
   }
 };
+
+twoSum([2, 7, 11, 15], 9);
